9362af39001f908d12a6d801f36f8ce5890eccf7
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-02-06 19:53:04 +0530

    latest updated ver

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
+import os
 import pandas as pd
 import numpy as np 
 import time 
 import datetime
 
+CHROMEDRIVER_PATH = r'C:\Program Files\chromedriver_win32\chromedriver.exe'
+WINDOW_SIZE = "1920,1080"
+chrome_options = Options()
+
+# chrome_options.add_argument("--headless")
+chrome_options.binary_location = r"C:\Users\TAMANG\Downloads\Win_1216615_chrome-win\chrome-win\chrome.exe"
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+
+service = Service(CHROMEDRIVER_PATH)
+
 def main():
 
     dff = pd.DataFrame(columns = ['Job Title', 'Description', 'Experience Reqd', 'Company', 'City', 'Salary Range', 'Date Posted', 'URL'])
     
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service = service, options = chrome_options)
 
     url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=&txtLocation=India#'
     driver.get(url)
 
     # print(driver.page_source) 
     
+    time.sleep(10)
+
     try:
        driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/table/tbody/tr/td[2]/div/span').click()
     except Exception as e:
         print('EXCEPTION OCCURED')
         pass
-
-    time.sleep(5)
-
-    soup = BeautifulSoup(driver.page_source, 'html5lib')
-    
-    # print(soup.encode('utf-8'))
-
-    result = soup.find('ul', class_='new-joblist')
-    result2 = result.find_all('li', class_='clearfix job-bx wht-shd-bx')
     
     page_counter = 0
     
     # print(result2)
-    pages = np.arange(1, 25)
+    pages = np.arange(1, 10)
 
     exception = 0
 
-    for page in pages:
+    for pages in pages:
+
         if page_counter == 0:
             next_counter = 0 
         else:
@@ -49,23 +57,49 @@ def main():
         page_next_counter = np.arange(2,12)
 
         for page_next in page_next_counter:
+    
+            try:
+                driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/table/tbody/tr/td[2]/div/span').click()
+            except Exception as e:
+                print('EXCEPTION OCCURED \n  x not present in the screen \n')
+                pass        
+
+            soup = BeautifulSoup(driver.page_source, 'html5lib')
+            
+            # print(soup.encode('utf-8'))
+
+            try:
+                    driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/table/tbody/tr/td[2]/div/span').click()
+            except Exception as e:
+                print('EXCEPTION OCCURED \n  x not present in the screen \n')
+                pass
+            try:
+                driver.find_element(By.XPATH, '//*[@id="closeSpanId"]').click()
+            except Exception as e:
+                print('EXCEPTION OCCURED \n  x ver 2 not present in the screen \n')
+                pass
+
+            result = soup.find('ul', class_='new-joblist')
+            result2 = result.find_all('li', class_='clearfix job-bx wht-shd-bx')
+            
             for i in result2:
+                
                 # TITLE
                 title = i.find('a')
-                title = title.text
-                print(title.encode('utf-8'))
+                title = title.text.strip()
+                # print(title.encode('utf-8'))
                 
                 # Description
                 description = i.find('label').next_sibling.strip()
-                print(description)
+                # print(description)
 
                 # COMPANY
                 text = i.find('h3', class_='joblist-comp-name')
                 text = text.text
                 initial_company = text.find('(')
                 Company = text[:initial_company]
-                Company = Company.strip()
-                print(Company)
+                Company = Company.strip()   
+                # print(Company)
 
                 # Exp
                 Mat_icons = i.find_all('i', class_='material-icons')
@@ -80,7 +114,7 @@ def main():
                 # Date Posted
                 Date = i.find('span', class_='sim-posted')
                 Date = Date.text.strip()
-                print(Date)
+                # print(Date)
 
                 URL = i.find('a').get('href')
                 # print(URL)
@@ -94,22 +128,29 @@ def main():
                     Salary = 'Not Mentioned'
                 
                 dff = pd.concat([dff, pd.DataFrame([[title, description , Exp, Company, City, Salary, Date, URL]], columns = ['Job Title','Description', 'Experience Reqd', 'Company', 'City', 'Salary Range', 'Date Posted', 'URL'])], ignore_index=True)
+                
+            print(dff)
 
-                dff.to_excel('TimesJobs_' + str(datetime.date.today()) + '.xlsx')
+            dff.to_excel(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', "TimesJobs_" + str(datetime.date.today()) + ".xlsx"), index=False)
             
-            dff.to_excel('TimesJobs_' + str(datetime.date.today()) + '.xlsx')
             driver.execute_script("window.scrollTo(0,(document.body.scrollHeight))")
-            scroll_time = 1
+            
+            scroll_time = 2
             time.sleep(scroll_time)
+            
+            dff.to_excel(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', "TimesJobs_" + str(datetime.date.today()) + ".xlsx"), index=False)
             
             page_counter = page_counter + 1
 
             final_page_next = next_counter + page_next
-            driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/section/div[2]/div[2]/div[4]/em[' + str(final_page_next) + ']/a').click()
+            try: 
+                driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/section/div[2]/div[2]/div[4]/em[' + str(final_page_next) + ']/a').click()
+            except Exception as e:
+                print('EXCEPTION OCCURED (UNABLE TO FIND THE BUTTON)\n', e, '\n********** KINDLY LOOK AFTER IT **********')
 
-            loading_time = 3
+            loading_time = 1
             time.sleep(loading_time)
 
             print('NUMBER OF EXCEPTIONS: ', exception)
-            
+
 main()
